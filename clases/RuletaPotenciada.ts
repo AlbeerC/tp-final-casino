2f6c39b38e20749a203c14d8ac68328f29c5f6ed
MatiCk272 <matiasceccarelli@gmail.com>
2024-11-29 21:31:53 -0300

    Creamos clase RuletaPotenciada y testeamos en el main

--- clases/RuletaPotenciada.ts
+++ clases/RuletaPotenciada.ts
@@ -1,11 +1,13 @@
-import { Ruleta } from "./Ruleta";
-import { Usuario } from "./Usuario";
 import * as readlineSync from "readline-sync";
 
-export class RuletaClasica extends Ruleta {
+import { Ruleta } from "./Ruleta";
+import { Usuario } from "./Usuario";
+
+export class RuletaPotenciada extends Ruleta {
+    private numerosPotenciados: number[] = [];
     private numeroGanador: number | null = null;
     private resultado: string = "";
-
+    
     constructor(nombre: string, apuestaMinima: number, apuestaMaxima: number) {
         super(nombre, apuestaMinima, apuestaMaxima);
     }
@@ -21,12 +23,14 @@ export class RuletaClasica extends Ruleta {
             return;
         }
 
+        this.generarNumerosPotenciados();
+
         const numerosElegidos: number[] = [];
 
         while (true) {
             const numero = readlineSync.questionInt(`Elige un numero del 0 al ${this.numeros.length - 1} (o presiona -1 para terminar): `);
 
-            if (numero === -1) break; // Salir del bucle si elige -1
+            if (numero === -1) break;
 
             if (numero < 0 || numero > this.numeros.length) {
                 console.log(`Número inválido. Debe estar entre 0 y ${this.numeros.length - 1}.`);
@@ -34,14 +38,13 @@ export class RuletaClasica extends Ruleta {
             }
 
             if (!numerosElegidos.includes(numero)) {
-                numerosElegidos.push(numero); // Agregar el número si no está en la lista
+                numerosElegidos.push(numero);
                 console.log(`Número ${numero} añadido a tu jugada.`);
             } else {
                 console.log(`El número ${numero} ya ha sido elegido.`);
             }
         }
 
-        // Calcular la apuesta total
         const apuestaTotal: number = numerosElegidos.length * apuesta;
 
         if (numerosElegidos.length === 0) {
@@ -49,14 +52,19 @@ export class RuletaClasica extends Ruleta {
             return;
         }
 
-        usuario.ajustarDinero(-apuestaTotal); // Descontar la apuesta total
-        this.numeroGanador = Math.floor(Math.random() * this.numeros.length - 1); // Número aleatorio entre 0 y 36
+        usuario.ajustarDinero(-apuestaTotal);
+        this.numeroGanador = Math.floor(Math.random() * this.numeros.length - 1);
 
-        // Verificar si el número ganador está entre los números elegidos
         if (numerosElegidos.includes(this.numeroGanador)) {
-            const ganancia = apuesta * this.numeros.length - 1;
+            let ganancia: number = 0;
+            if (this.numerosPotenciados.includes(this.numeroGanador)) {
+                ganancia = apuesta * (this.numeros.length - 1) * 5;
+                this.resultado = `¡Felicidades! Uno de tus números (${this.numeroGanador}) estaba potenciado y fue el ganador. Has ganado ${ganancia} fichas.`;
+            } else {
+                ganancia = ganancia = apuesta * this.numeros.length - 1;
+                this.resultado = `¡Felicidades! Uno de tus números (${this.numeroGanador}) fue el ganador. Has ganado ${ganancia} fichas.`;
+            }
             usuario.ajustarDinero(ganancia);
-            this.resultado = `¡Felicidades! Uno de tus números (${this.numeroGanador}) fue el ganador. Has ganado ${ganancia} fichas.`;
         } else {
             this.resultado = `Has perdido tu apuesta. El número ganador fue ${this.numeroGanador}. Mejor suerte la próxima vez.`;
         }
@@ -67,6 +75,15 @@ export class RuletaClasica extends Ruleta {
             console.log(this.resultado);
         } else {
             console.log("No hay resultado para mostrar aún");
+        }
+    }
+    
+    generarNumerosPotenciados(): void {
+        for (let i: number = 0; i < 5; i++) {
+            const numeroAleatorio: number = Math.floor(Math.random() * this.numeros.length);
+            if (!this.numerosPotenciados.includes(numeroAleatorio)) {
+                this.numerosPotenciados.push(numeroAleatorio);
+            }
         }
     }
 }
--- main.ts
+++ main.ts
@@ -1,14 +1,19 @@
 import { Casino } from "./clases/Casino";
 import { RuletaClasica } from "./clases/RuletaClasica";
+import { RuletaPotenciada } from "./clases/RuletaPotenciada";
 import { Usuario } from "./clases/Usuario";
 
-const casino = new Casino("Casino", 123, "Olavarria")
-const ruletaClasica = new RuletaClasica("Ruleta clasica", 1, 30)
-const user = new Usuario("Alberto", 400)
+const casino = new Casino("Casino", 123, "Olavarria");
+const ruletaClasica = new RuletaClasica("Ruleta clasica", 1, 30);
+const user = new Usuario("Alberto", 400);
+const ruletaPotenciada = new RuletaPotenciada("Ruleta Potenciada", 1, 30);
 
-casino.crearJuego(ruletaClasica)
+casino.crearJuego(ruletaClasica);
 
-user.verDinero()
-ruletaClasica.iniciarTirada(user, 10)
-ruletaClasica.mostrarResultado()
-user.verDinero()
+user.verDinero();
+/* ruletaClasica.iniciarTirada(user, 10);
+ruletaClasica.mostrarResultado(); */
+user.verDinero();
+
+ruletaPotenciada.iniciarTirada(user, 5);
+ruletaPotenciada.mostrarResultado();
