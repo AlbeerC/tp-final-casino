b51283845b0567ca4dbfb5ca4073632eed1c17e9
Juan <juanlopey22@gmail.com>
2024-12-02 19:54:43 -0300

    Agregamos la clase padre Tragamoneda

---
+++ clases/Tragamoneda.ts
@@ -0,0 +1,14 @@
+import { Juego } from "./Juego";
+import { Usuario } from "./Usuario";
+
+export abstract class Tragamoneda extends Juego {
+
+    constructor(nombre:string, apuestaMinima:number, apuestaMaxima:number){
+        super(nombre, apuestaMaxima, apuestaMinima)
+    }
+
+    abstract iniciarTirada(usuario: Usuario, apuesta: number): void;
+    abstract mostrarResultado(): void;
+    abstract calcularGanancia():void;
+
+}
